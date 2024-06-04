0296d9696c1f9f377fc1a8ee763c1861d03de4da
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-06-04 15:30:19 +0300

    PCABO3-10754: Обновил TableViewUpdater

--- Sources/ArchitectureTableView/TableViewModule/TableView.swift
+++ Sources/ArchitectureTableView/TableViewModule/TableView.swift
@@ -7,27 +7,40 @@
 
 import Architecture
 import UIKit
+import Components
 
 public final class TableView: UITableView, ViewProtocol {
     
-    // MARK: - Properties
-
+    // MARK: - Public properties
+    
     public struct ViewProperties {
-        let dataStorage: GenericTableViewDataStorage
-        let setupTableView: Closure<UITableView>
-        let rowHeight: CGFloat
-        var isReload: Bool
-        
+        public let allowsSelection: Bool
+        public let separatorColor: UIColor
+        public let backgroundColor: UIColor
+        public let rowHeight: CGFloat
+        public let separatorInset: UIEdgeInsets
+        public let dataSources: UITableViewDataSource
+        public let delegate: UITableViewDelegate
+        public var didTapGesture: ClosureEmpty = {}
+
         public init(
-            dataStorage: GenericTableViewDataStorage = .empty,
-            setupTableView: @escaping Closure<UITableView> = { _ in },
-            rowHeight: CGFloat = 72,
-            isReload: Bool = false
+            allowsSelection: Bool = true,
+            separatorColor: UIColor = .clear,
+            backgroundColor: UIColor = .clear,
+            rowHeight: CGFloat,
+            separatorInset: UIEdgeInsets = .zero,
+            dataSources: UITableViewDataSource,
+            delegate: UITableViewDelegate,
+            didTapGesture: @escaping ClosureEmpty = {}
         ) {
-            self.dataStorage = dataStorage
-            self.setupTableView = setupTableView
+            self.allowsSelection = allowsSelection
+            self.separatorInset = separatorInset
+            self.separatorColor = separatorColor
+            self.backgroundColor = backgroundColor
             self.rowHeight = rowHeight
-            self.isReload = isReload
+            self.dataSources = dataSources
+            self.delegate = delegate
+            self.didTapGesture = didTapGesture
         }
     }
     
@@ -36,22 +49,18 @@ public final class TableView: UITableView, ViewProtocol {
     // MARK: - Life cycle
     
     public init(
-        style: UITableView.Style = .plain,
-        viewProperties: ViewProperties
+        viewProperties: ViewProperties,
+        style: UITableView.Style
     ) {
         self.viewProperties = viewProperties
         
         super.init(frame: .zero, style: style)
-    
-        self.viewProperties.setupTableView(self)
-        tableFooterView = UIView()
-        keyboardDismissMode = .onDrag
-        separatorStyle = .none
         
-        setup()
+        setupTableView()
+        addGesture()
     }
     
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
@@ -60,22 +69,44 @@ public final class TableView: UITableView, ViewProtocol {
     public func update(with viewProperties: ViewProperties) {
         self.viewProperties = viewProperties
         
-        setup()
-        reload()
+        setupTableView()
+        reloadData()
     }
     
-    // MARK: - Private properties
+    // MARK: - Private methods
     
-    private func setup() {
-        delegate = viewProperties.dataStorage.tableViewDelegate
-        dataSource = viewProperties.dataStorage.tableViewDataSource
+    private func setupTableView() {
+        delegate = viewProperties.delegate
+        dataSource = viewProperties.dataSources
+        reloadData()
+        backgroundColor = viewProperties.backgroundColor
         estimatedRowHeight = viewProperties.rowHeight
-        rowHeight = UITableView.automaticDimension
+        separatorColor = viewProperties.separatorColor
+        separatorInset = viewProperties.separatorInset
+        allowsSelection = viewProperties.allowsSelection
+        showsVerticalScrollIndicator = false
+        cornerRadius(
+            radius: 10,
+            direction: .allCorners,
+            clipsToBounds: true
+        )
+        
+        if #available(iOS 15.0, *) {
+            sectionHeaderTopPadding = 0
+        }
     }
     
-    private func reload() {
-        guard viewProperties.isReload == true else { return }
-        self.viewProperties.dataStorage.registerFor(self)
-        self.reloadData()
+    private func addGesture() {
+        let hideGuest = UITapGestureRecognizer(
+            target: self,
+            action: #selector(didTapGesture)
+        )
+        hideGuest.cancelsTouchesInView = false
+        addGestureRecognizer(hideGuest)
+    }
+    
+    @objc
+    private func didTapGesture() {
+        viewProperties.didTapGesture()
     }
 }
--- Sources/ArchitectureTableView/TableViewModule/TableViewUpdater.swift
+++ Sources/ArchitectureTableView/TableViewModule/TableViewUpdater.swift
@@ -1,47 +1,31 @@
 //
 //  TableViewUpdater.swift
+//  
 //
-//
-//  Created by user on 31.05.2024.
+//  Created by user on 04.06.2024.
 //
 
-import UIKit
 import Architecture
+import UIKit
 
-public final class TableViewUpdater: ViewUpdater<TableView> {
+final public class TableViewUpdater: ViewUpdater<TableView> {
     
-    // MARK: - Public properties
-    
-    public struct TableViewModel {
-        let rowHeight: CGFloat
-        
-        public init(rowHeight: CGFloat = 64) {
-            self.rowHeight = rowHeight
-        }
-    }
+    //MARK: - Public properties
     
     public enum State {
-        case createViewProperties(TableViewModel)
-        case reloadData([GenericTableViewSectionModel])
-        case updateRow([IndexPath])
-        case setSection([GenericTableViewSectionModel])
+        case updateViewProperties(TableView.ViewProperties)
+        case reloadData
     }
     
     public var state: State? {
-        didSet { stateManager() }
+        didSet { self.stateManager() }
     }
     
     public var viewProperties: TableView.ViewProperties
     
-    // MARK: - Private properties
-    
-    private lazy var dataStorage = GenericTableViewDataStorage.empty
-    private lazy var dataSource = dataStorage.tableViewDataSource as? GenericTableViewDataSource
-    private lazy var delegate = dataStorage.tableViewDelegate as? GenericTableViewDelegate
-    
     // MARK: - Life cycle
     
-    override public init(
+    override init(
         viewProperties: TableView.ViewProperties,
         update: @escaping Closure<TableView.ViewProperties>
     ) {
@@ -56,44 +40,14 @@ public final class TableViewUpdater: ViewUpdater<TableView> {
     // MARK: - Private methods
     
     private func stateManager() {
-        guard let state else { return }
+        guard let state = self.state else { return }
         
         switch state {
-        case .createViewProperties(let tableViewModel):
-            viewProperties = TableView.ViewProperties(
-                dataStorage: dataStorage,
-                setupTableView: setupTableView,
-                rowHeight: tableViewModel.rowHeight,
-                isReload: false
-            )
+        case .updateViewProperties(let viewProperties):
+            self.viewProperties = viewProperties
             update(viewProperties)
-            setupDataStorage()
-        case .reloadData(let sections):
-            reloadData(with: sections)
-        case .updateRow(let indexPath):
-            updateRow(with: indexPath)
-        case .setSection(let sections):
-            dataStorage.update(with: sections)
+        case .reloadData:
+            update(viewProperties)
         }
-    }
-    
-    private func setupTableView(with tableView: UITableView) {
-        dataStorage.registerFor(tableView)
-        tableView.reloadData()
-    }
-    
-    private func reloadData(with sections: [GenericTableViewSectionModel]) {
-        dataStorage.update(with: sections)
-        viewProperties.isReload = true
-        update(viewProperties)
-        viewProperties.isReload = false
-    }
-    
-    private func updateRow(with indexPath: [IndexPath]) {
-        dataSource?.updateRow(with: indexPath)
-    }
-    
-    private func setupDataStorage() {
-        dataStorage.defaultSeparatorStyle = .none
     }
 }
