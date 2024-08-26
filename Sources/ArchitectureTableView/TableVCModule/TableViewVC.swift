6b0fc862899eac26d945b43037dc774d3b81c388
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-08-26 13:11:10 +0300

    PCABO3-10760: Добавил обновление NavigationBar.ViewProperties

--- Sources/ArchitectureTableView/TableVCModule/TableViewVC.swift
+++ Sources/ArchitectureTableView/TableVCModule/TableViewVC.swift
@@ -38,10 +38,12 @@ public final class TableViewVC: UIViewController, ViewProtocol {
         viewProperties: ViewProperties
     ) {
         self.viewProperties = viewProperties
+        
         super.init(nibName: nil, bundle: nil)
-        addTableView()
-        addConfirmButtonView()
-        addActivityIndicator()
+        
+        addTableView(viewProperties: viewProperties)
+        addConfirmButtonView(viewProperties: viewProperties)
+        addActivityIndicator(viewProperties: viewProperties)
         setupNavigationBar(viewProperties: viewProperties)
     }
     
@@ -51,31 +53,60 @@ public final class TableViewVC: UIViewController, ViewProtocol {
     
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         setupNavigationBar(viewProperties: viewProperties)
     }
     
     // MARK: - Public methods
     
     public func update(with viewProperties: ViewProperties) {
+        updateTableViewIfNeeded(newViewProperties: viewProperties)
+        updateConfirmButtonIfNeeded(newViewProperties: viewProperties)
+        updateActivityIndicatorIfNeeded(newViewProperties: viewProperties)
+        updateNavigationBarIfNeeded(newViewProperties: viewProperties)
+        
         self.viewProperties = viewProperties
     }
-
+    
     // MARK: - Private methods
     
     private func setupNavigationBar(viewProperties: ViewProperties) {
-        guard let navigationBarViewProperties = viewProperties.navigationBarViewProperties else { return }
+        guard let navigationBarViewProperties = viewProperties.navigationBarViewProperties
+        else { return }
+        
         let navigationBar = navigationController as? NavigationBar
         navigationBar?.update(with: navigationBarViewProperties)
     }
     
-    private func addTableView() {
+    private func updateNavigationBarIfNeeded(newViewProperties: ViewProperties) {
+        guard newViewProperties.navigationBarViewProperties != viewProperties.navigationBarViewProperties,
+              let navigationBarViewProperties = newViewProperties.navigationBarViewProperties
+        else { return }
+        
+        let navigationBar = navigationController as? NavigationBar
+        navigationBar?.update(with: navigationBarViewProperties)
+    }
+    
+    private func addTableView(viewProperties: ViewProperties) {
         view.addSubview(viewProperties.tableView)
         viewProperties.tableView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
     
-    private func addConfirmButtonView() {
+    private func updateTableViewIfNeeded(newViewProperties: ViewProperties) {
+        guard newViewProperties.tableView != viewProperties.tableView else { return }
+        
+        viewProperties.tableView.snp.removeConstraints()
+        viewProperties.tableView.removeFromSuperview()
+        
+        view.addSubview(newViewProperties.tableView)
+        newViewProperties.tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func addConfirmButtonView(viewProperties: ViewProperties) {
         guard let confirmButtonView = viewProperties.confirmButtonView else { return }
         
         view.addSubview(confirmButtonView)
@@ -84,8 +115,36 @@ public final class TableViewVC: UIViewController, ViewProtocol {
         }
     }
     
-    private func addActivityIndicator() {
+    private func updateConfirmButtonIfNeeded(newViewProperties: ViewProperties) {
+        guard newViewProperties.confirmButtonView != viewProperties.confirmButtonView,
+              let confirmButtonView = newViewProperties.confirmButtonView
+        else { return }
+        
+        viewProperties.confirmButtonView?.snp.removeConstraints()
+        viewProperties.confirmButtonView?.removeFromSuperview()
+        
+        view.addSubview(confirmButtonView)
+        confirmButtonView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func addActivityIndicator(viewProperties: ViewProperties) {
         guard let activityIndicator = viewProperties.activityIndicator else { return }
+        
+        view.addSubview(activityIndicator)
+        activityIndicator.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    private func updateActivityIndicatorIfNeeded(newViewProperties: ViewProperties) {
+        guard newViewProperties.activityIndicator != viewProperties.activityIndicator,
+              let activityIndicator =  newViewProperties.activityIndicator
+        else { return }
+        
+        viewProperties.activityIndicator?.snp.removeConstraints()
+        viewProperties.activityIndicator?.removeFromSuperview()
         
         view.addSubview(activityIndicator)
         activityIndicator.snp.makeConstraints { make in
@@ -96,5 +155,27 @@ public final class TableViewVC: UIViewController, ViewProtocol {
     @objc private func backTapped() {
         view.endEditing(true)
         navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - NavigationBar.ViewProperties Extension
+ 
+extension NavigationBar.ViewProperties: Equatable {
+    public static func == (
+        lhs: NavigationBar.ViewProperties,
+        rhs: NavigationBar.ViewProperties
+    ) -> Bool {
+        return lhs.leftBarButtonItems == rhs.leftBarButtonItems 
+            && lhs.rightBarButtonItems == rhs.rightBarButtonItems
+            && lhs.titleView == rhs.titleView
+            && lhs.title == rhs.title
+            && lhs.largeTitleDisplayMode == rhs.largeTitleDisplayMode
+            && lhs.prefersLargeTitles == rhs.prefersLargeTitles
+            && lhs.standartAppearance == rhs.standartAppearance
+            && lhs.compactAppearance == rhs.compactAppearance
+            && lhs.scrollEdgeAppearance == rhs.scrollEdgeAppearance
+            && lhs.searchController == rhs.searchController
+            && lhs.hidesSearchBarWhenScrolling == rhs.hidesSearchBarWhenScrolling
+            && lhs.isNavigationBarHidden == rhs.isNavigationBarHidden
     }
 }
