31d12d9556fa594699230705026c752a6d0bb5e5
KaluginAA <KaluginAA@akbarsdigital.ru>
2024-08-26 16:42:43 +0300

    PCABO3-10780: Добавил сервис для создания секций

---
+++ Sources/ArchitectureTableView/Table/Services/SectionRowModelService.swift
@@ -0,0 +1,36 @@
+//
+//  SectionRowModelService.swift
+//
+//
+//  Created by user on 26.08.2024.
+//
+
+import UIKit
+import DesignSystem
+
+public struct SectionRowModelService {
+    
+    public func createSections(
+        from rows: [DSRowModel],
+        rowsHeight: CGFloat? = nil,
+        cellBackgroundColor: UIColor = .white
+    ) -> [SectionModel] {
+        return rows.map { row in
+            let cell = DSCreationRowsViewService().createViewRowWithBlocks(
+                leading: row.leading,
+                center: row.center,
+                trailing: row.trailing,
+                centralBlockAlignment: row.centralBlockAlignment
+            )
+            let cellModel = CellModel(
+                view: cell,
+                selectionStyle: .none,
+                height: rowsHeight,
+                didTap: nil,
+                backgroundColor: cellBackgroundColor
+            )
+            
+            return SectionModel(cells: [cellModel])
+        }
+    }
+}
